bc10463bf7692d382e61abe91100937107580431
George Shopov <gsshopov@gmail.com>
2014-04-21 00:06:34 +0300

    Change BandOfRobbers according to the adjusted description

--- exercises/exercise3/BandOfRobbers.cpp
+++ exercises/exercise3/BandOfRobbers.cpp
@@ -1,170 +1,141 @@
 #include <iostream>
 #include <cstring>
+
 using namespace std;
 
-
-struct robber
+struct Robber
 {
     char name[51];
     double rate;
     bool inPrison;
 
-    robber(const char* _name = " ", double _rate = 0.0, bool _inPrison = false)
+    Robber(const char* _name="", double _rate=0.0, bool _inPrison=false):
+        rate(_rate), inPrison(_inPrison)
     {
         strcpy (name, _name);
-        rate = _rate;
-        inPrison = _inPrison;
     }
 
     void print () const
     {
-        cout << "Name: " << name << endl
-             << "Rate: " << rate << endl
-             << "inPrison: " << boolalpha << inPrison << endl << endl;
+        cout << "Name: "     << name << endl
+             << "Rate: "     << rate << endl
+             << "inPrison: " << boolalpha << inPrison << endl;
     }
 };
-
 
 class BankRobbers
 {
-    private:
-        robber group[10];
-        int size;
-        double maxLoot;
+public:
+    BankRobbers():
+        size(0), maxLoot(0)
+    { }
 
-		// A hidden method used for calculating the maxLoot property
-        double getMaxLoot ()
+    void addRobber();
+
+    void addRobber(const Robber& robber)
+    {
+        if (size < 10)
         {
-            double loot = 0;
-            for (int i = 0; i < size; i ++)
-                if (group[i].inPrison == false)
-                    loot += group[i].rate * 0.8;
-            return loot;
+            group[size] = robber;
+            size++;
         }
 
-    public:
+        maxLoot = getMaxLoot();
+    }
 
-        BankRobbers ();
-        BankRobbers (const BankRobbers &b);
-        BankRobbers &operator= (const BankRobbers &b);
+    void setName(char* name, int index)
+    {
+        strcpy(group[index].name, name);
+    }
 
-	//---- Mutators -------
-		void addRobber (const robber &r);	// Adds the given robber to the group if the group isn't full
-        void addRobber();					// Adds a newly created roobber to the group if not full
+    void setRate(double rate, int index)
+    {
+        group[index].rate = rate;
+    }
 
+    void setInPrison(bool inPrison, int index)
+    {
+        group[index].inPrison = inPrison;
+    }
 
-        void setName (char* na, int index) { strcpy(group[index].name,na);  }
-        void setRate (double r, int index) { group[index].rate = r; }
-        void setInPrison (bool p, int index) { group[index].inPrison = p; }
-	//---- End Of Mutators -------
+    const char* getName(int index)
+    {
+        return group[index].name;
+    }
 
+    double getRate(int index) const
+    {
+        return group[index].rate;
+    }
 
-        const char *getName (int index) { return group[index].name; }
-        double getRate (int index) const { return group[index].rate; }
-        bool getInPrison (int index) const { return group[index].inPrison; }
+    bool getInPrison(int index) const
+    {
+        return group[index].inPrison;
+    }
 
-        void print () const;
+    void print () const
+    {
+        cout << "Max loot: " << maxLoot << endl;
 
+        for (int i =0 ; i < size ; i ++)
+        {
+            group[i].print();
+        }
+    }
+
+private:
+    int size;
+    double maxLoot;
+    Robber group[10];
+
+    double getMaxLoot ()
+    {
+        double loot = 0;
+        for (int i = 0; i < size; i ++)
+            if (group[i].inPrison == false)
+                loot += group[i].rate * 0.8;
+        return loot;
+    }
 };
-
-// --- Constructors ----
-BankRobbers::BankRobbers() : size(0), maxLoot(0) {}	// Nekvi shashmi tuka (vijte si uchebnika)
-
-BankRobbers::BankRobbers (const BankRobbers &b) : size(b.size), maxLoot(b.maxLoot)
-{
-    for(int i = 0; i < size; i++)
-    {
-		group[i] = b.group[i];
-	}
-}
-
-BankRobbers& BankRobbers::operator=(const BankRobbers &b)
-{
-    if (this != &b)
-    {
-		size = b.size;
-
-		for(int i = 0; i < size; i++)
-			group[i] = b.group[i];
-
-		maxLoot = b.maxLoot;
-    }
-    return *this;
-}
-// --- End Of Constructors ---
-
-
-void BankRobbers::addRobber(const robber &r)
-{
-    if (size < 10)
-    {
-		group[size] = r;
-		size ++;
-    }
-    else
-		cout << "The group is full!" << endl;
-
-    maxLoot = getMaxLoot();
-}
 
 void BankRobbers::addRobber()
 {
     if (size < 10)
     {
-		cout << "Name: ";
-		cin.getline(group[size].name,50);
+        cout << "Name: ";
+        cin.getline(group[size].name, 50);
 
-		cout << "Rate: ";
-		cin >> group[size].rate;
+        cout << "Rate: ";
+        cin >> group[size].rate;
 
-		char c;
-		cout << "Is the " << group[size].name << " in prison : (y/n) ";
-		while (c != 'y' && c != 'n')
-		{
-			cin >>c;
-			if (c == 'n')
-			group[size].inPrison = false;
-			else
-			if (c == 'y')
-				group[size].inPrison = true;
-			else
-			cout << "Wrong command!" << endl;
-		}
-		size ++;
-		cin.get();
-	}
-    else
-		cout << "You cant add robber" << endl;
+        char c;
+        cout << "Is the " << group[size].name << " in prison : (y/n) ";
+
+        while (c != 'y' && c != 'n')
+        {
+            cin >>c;
+            if (c == 'n')
+                group[size].inPrison = false;
+            else if (c == 'y')
+                group[size].inPrison = true;
+            else
+                cout << "Wrong command!" << endl;
+        }
+
+        size ++;
+        cin.get();
+    }
 
     maxLoot = getMaxLoot();
 }
 
-void BankRobbers::print () const
-{
-    cout << "Max loot: " << maxLoot << endl;
-
-    for (int i =0 ; i < size ; i ++)
-    {
-		group[i].print();	// Using the print method implemented in the struct
-	}
-}
-
-
-
 int main ()
 {
-    robber p("Peio",10,false);
+    Robber p("Peio", 10, false);
     BankRobbers g;
     g.addRobber(p);
-    //g.addRobber();
 
     g.print();
-    //cout << endl << endl;
-
-    //BankRobbers g2(g);
-    //g2.addRobber();
-    //g2.print();
-
 
     return 0;
 }
